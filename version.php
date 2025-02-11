1c9586a8ed635e89195fd68b237191eb619e84f0
Robert Russo <rrusso@lsu.edu>
2025-02-11 12:00:24 -0600

    bump version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'enrol_workdaystudent';
-$plugin->version = 2024121700;
+$plugin->version = 2025021100;
 $plugin->requires = 2020010100;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '0.01-Patagonian-Penguin';
